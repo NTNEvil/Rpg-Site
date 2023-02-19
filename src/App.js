526913df0d4acd0eb0d73adf24ad05447edc1a07
Momessito <pinhoes1000@gmail.com>
2023-02-19 20:28:51 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
+import Map from './map.png'
+import mySound from './music.mp3'
+import ReactAudioPlayer from 'react-audio-player';
+
+import Nav from './components/nav';
 
 function App() {
+
+
   return (
+    
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+
+      <h2 className='Login'>Log In</h2>
+      <Nav></Nav>
+      <img src={Map} className='Map'/>
+      <div className='light'></div>
+      <h1 className='title'>🔮 Earth of Dungeons 🔮</h1>
+      <ReactAudioPlayer className='music'
+  src={mySound}
+  autoPlay={true}
+  loop={true}
+  controls={true}
+  volume={0.1}
+/>
     </div>
   );
 }
